8295f2e6f68d553e8f8a994a9dfb73a993f90daf
José Andrés Salmerón Bueno <jasalmeron@gmail.com>
2017-10-17 14:40:47 +0200

    Apply changes to pass TweetServiceTest tests

--- src/main/java/com/scmspain/domain/services/TweetService.java
+++ src/main/java/com/scmspain/domain/services/TweetService.java
@@ -6,6 +6,9 @@ import com.scmspain.domain.validation.TweetValidatorChain;
 import org.springframework.boot.actuate.metrics.writer.Delta;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -27,41 +30,61 @@ public class TweetService {
     }
 
     /**
-      Push tweet to repository
-      Parameter - publisher - creator of the Tweet
-      Parameter - text - Content of the Tweet
-      Result - recovered Tweet
-    */
+     * Push tweet to repository
+     * Parameter - publisher - creator of the Tweet
+     * Parameter - text - Content of the Tweet
+     * Result - recovered Tweet
+     */
     public void publishTweet(String publisher, String text) {
         Tweet tweet = new Tweet(publisher, text);
         tweetValidator.validate(tweet);
-        this.metricWriter.increment(new Delta<Number>("published-tweets", 1));
         this.entityManager.persist(tweet);
+        this.metricWriter.increment(new Delta<Number>("published-tweets", 1));
     }
 
 
     /**
-      Recover tweet from repository
-      Parameter - id - id of the Tweet to retrieve
-      Result - retrieved Tweet
-    */
+     * Recover tweet from repository
+     * Parameter - id - id of the Tweet to retrieve
+     * Result - retrieved Tweet
+     */
     public Tweet getTweet(Long id) {
-      return this.entityManager.find(Tweet.class, id);
+        return this.entityManager.find(Tweet.class, id);
     }
 
     /**
-      Recover tweet from repository
-      Parameter - id - id of the Tweet to retrieve
-      Result - retrieved Tweet
-    */
+     * Recover all tweets from repository
+     * Result - retrieved list of published Tweet
+     */
     public List<Tweet> listAllTweets() {
-        List<Tweet> result = new ArrayList<Tweet>();
+        List<Tweet> tweets = listTweets(false);
         this.metricWriter.increment(new Delta<Number>("times-queried-tweets", 1));
-        TypedQuery<Long> query = this.entityManager.createQuery("SELECT id FROM Tweet AS tweetId WHERE pre2015MigrationStatus<>99 ORDER BY id DESC", Long.class);
-        List<Long> ids = query.getResultList();
-        for (Long id : ids) {
-            result.add(getTweet(id));
-        }
-        return result;
+        return tweets;
+    }
+
+    /**
+     * Recover all discarded tweets from repository
+     * @return retrieved list of discarded Tweet
+     */
+    public List<Tweet> listAllDiscardedTweets() {
+        List<Tweet> tweets = listTweets(true);
+        this.metricWriter.increment(new Delta<Number>("times-queried-ignored-tweets", 1));
+        return tweets;
+    }
+
+    private List<Tweet> listTweets(boolean ignored) {
+        return this.entityManager.createQuery("SELECT tweet FROM Tweet AS tweet WHERE pre2015MigrationStatus<>99 AND discarded=" + ignored +" ORDER BY id DESC", Tweet.class).getResultList();
+    }
+
+    /**
+     * Discard tweet
+     * @param id identifier of the tweet to be discarded.
+     */
+    public void discardTweet(Long id) {
+        Assert.notNull(id, "id can't be null to delete a tweet");
+        Tweet tweet = getTweet(id);
+        tweet.setDiscarded(true);
+        this.entityManager.merge(tweet);
+        metricWriter.increment(new Delta<Number>("published-tweets", -1));
     }
 }
